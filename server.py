adc842633b00e8edfc7d209297a12c5174a4fe42
An0rak <phos4es@gmail.com>
2017-05-14 00:36:44 -0400

    guugugh

--- server.py
+++ server.py
@@ -3,29 +3,17 @@ import socket
 
 s = socket.socket()
 host = socket.gethostname()
-port = 37531
+port = 25565
 s.bind((host,port))
-
-def snapshot():
-	fp = open('image.jpeg','w')
-	while True:
-		recv_string = c.recv(1024)
-		if not recv_string:
-			break
-		fp.write(recv_string)
-	fp.close()
-	print "image recieved"
-
-def checkstring():
-	if send_string == "snapshot":
-		snapshot()
-
 s.listen(5)
 c, addr = s.accept()
 print('CONN', addr)
-while True:
-	send_string = raw_input("Type: ")
-	c.send(send_string)
-	checkstring()
-	print c.recv(1024)
-c.close()
+try:
+	while True:
+		send_string = raw_input(">> ")
+		c.send(send_string)
+		print c.recv(1024)
+	c.close()
+except Exception as e:
+	c.close()
+	print('Exiting')
